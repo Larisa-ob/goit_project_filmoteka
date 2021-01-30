bad19a2f34a4427e2537bac72235f103943ff11d
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2021-01-30 23:20:29 +0200

    ferst

---
+++ src/js/components/headers-tpl.js
@@ -0,0 +1,63 @@
+const headerTemplates = {
+  homeHeader: `<div class=" header home-header header-container">
+  <div class="header-nav-container">
+    <a href="" class="header-logo logo link header-logo-js">
+    <span class="header-logo-img"></span>
+    <span class="header-logo-txt">Filmoteka</span></a>
+  
+<nav class="header-navigation">
+  <ul class="header-navigation-list">
+    <li class="header-navigation-list-item"><a href=""
+        class="navigation-list-item-link link navigation-list-item-link-home current">HOME</a></li>
+    <li class="header-navigation-list-item"><a href=""
+        class="navigation-list-item-link link navigation-list-item-link-my-library">MY LIBRARY</a>
+    </li>
+    <li class="header-navigation-list-item"><a href=""
+        class="navigation-list-item-link link navigation-list-item-link-join-us">join us</a>
+    </li>
+  </ul>
+</nav></div>
+  <form action="" class="header-search-form" id="search-form">
+    <input type="text" name="query" class="header-search-form-input" placeholder="Поиск фильмов">
+    <button class="header-search-form-btn" type="submit"></button>
+  </form>
+  <p id="header-search-warning-show">Search result not successful. Enter the correct movie name and</p>
+</div>`,
+  myLibraryHeader: `<div class=" header my-library-header header-container">
+  <div class="header-nav-container">
+    <a href="" class="header-logo logo link header-logo-js">
+      <span class="header-logo-img"></span>
+      <span class="header-logo-txt">Filmoteka</span></a>
+  <nav class="header-navigation">
+    <ul class="header-navigation-list">
+      <li class="header-navigation-list-item"><a href=""
+          class="navigation-list-item-link link navigation-list-item-link-home">HOME</a></li>
+      <li class="header-navigation-list-item"><a href=""
+          class="navigation-list-item-link link navigation-list-item-link-my-library current">MY LIBRARY</a>
+      </li>
+  
+    </ul>
+  </nav>
+  </div>
+  <ul class="header-button-list">
+    <li class="header-button-item"><button class="header-button header-button-watched is-active-btn">watched</button></li>
+    <li class="header-button-item"><button class="header-button header-button-queue">queue</button></li>
+  </ul>
+  
+</div>`,
+  modalHeader: `<div class="header modal-header header-container">
+  <div class="header-nav-container">
+    <a href="" class="header-logo logo link header-logo-js">
+      <span class="header-logo-img"></span>
+      <span class="header-logo-txt">Filmoteka</span></a>
+    <nav class="header-navigation">
+      <ul class="header-navigation-list">
+        <li class="header-navigation-list-item"><a href="" class="navigation-list-item-link link navigation-list-item-link-home">HOME</a></li>
+        <li class="header-navigation-list-item"><a href="" class="navigation-list-item-link link navigation-list-item-link-my-library">MY LIBRARY</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</div>`,
+};
+export default headerTemplates;
---
+++ src/js/modalMovie.js
@@ -0,0 +1,111 @@
+import headerTemplates from './components/headers-tpl';
+import movieDetailedCardTemplate from '../templates/details-modal.hbs';
+import refs from './refs';
+import {
+  userWatched,
+  updateUserWatched,
+  userQueue,
+  updateUserQueue,
+} from './userLibrary';
+
+const apiKey = 'ffddee44025dd24685ea61d637d56d24';
+const baseUrl = 'https://api.themoviedb.org/3/movie/';
+let movieId = 0;
+
+refs.filmsGallery = document.querySelector('.films-gallery-container');
+refs.body = document.querySelector('body');
+
+refs.filmsGallery.addEventListener('click', handleMovieDetails);
+
+function handleMovieDetails(event) {
+  if (event.target.parentNode.nodeName !== 'LI') {
+    return;
+  }
+  movieId = event.target.parentNode.dataset.id;
+  getMovieDetails(baseUrl, apiKey, movieId)
+    .then(data => {
+      const newData = {
+        id: data.id,
+        poster_path: data.poster_path,
+        title: data.title,
+        vote_average: data.vote_average,
+        vote_count: data.vote_count,
+        popularity: data.popularity,
+        original_title: data.original_title,
+        overview: data.overview,
+        genres: data.genres.slice(0, 3),
+      };
+
+      const modalMovieCard = movieDetailedCardTemplate(newData);
+      const markup = `<div class="modal-backdrop">
+    ${headerTemplates.modalHeader}
+    ${modalMovieCard}
+    <footer class="footer">
+      <div class="block">
+        <p class="text">&#169; 2020 | All Rights Reserved |</p>
+        <div class="mob-container">
+          <p class="develop-text">Developed with</p>
+          <span class="footer-heart-svg heart-svg"></span>
+          <a class="open-modal-develop" href="#">by GoIT Students</a>
+        </div>
+      </div>
+    </footer>
+    </div>`;
+      refs.body.insertAdjacentHTML('beforeend', markup);
+
+      window.addEventListener('keydown', onPressESC);
+      refs.modalCard = document.querySelector('.modal-backdrop');
+      refs.modalCard.addEventListener('click', closeOnClick);
+
+      // WATCHED BUTTON HANDLER
+      refs.addToWatchedBtn = document.querySelector('#js-watched-button');
+      refs.addToWatchedBtn.addEventListener('click', e => {
+        updateUserWatched(newData);
+      });
+
+      // QUEUE BUTTON HANDLER
+      refs.addToQueueBtn = document.querySelector('#js-queue-button');
+      refs.addToQueueBtn.addEventListener('click', e => {
+        updateUserQueue(newData);
+      });
+    })
+    .catch(error => console.log(error));
+}
+
+function getMovieDetails(baseUrl, apiKey, movieId) {
+  return fetch(`${baseUrl}${movieId}?api_key=${apiKey}&language=en-US`)
+    .then(response => response.json())
+    .catch(error => {
+      throw error;
+    });
+}
+
+function closeMovieDetails() {
+  console.log('closeMovieDetails');
+  refs.modalCard.remove();
+}
+
+function onPressESC(event) {
+  console.log('onPresEsc');
+  if (event.code === 'Escape') {
+    closeMovieDetails();
+    window.removeEventListener('keydown', onPressESC);
+  }
+}
+
+function closeOnClick(event) {
+  refs.modalDetailedCard = document.querySelector('.modal');
+  refs.modalInfo = document.querySelector('.modal-info');
+  refs.modalGenreInfo = document.querySelector('.modal-info-genres');
+
+  switch (event.target.parentNode) {
+    case refs.modalGenreInfo:
+    case refs.modalInfo:
+    case refs.modalDetailedCard:
+      closeMovieDetails();
+      refs.modalCard.removeEventListener('click', closeOnClick);
+      break;
+    default:
+      return;
+  }
+}
